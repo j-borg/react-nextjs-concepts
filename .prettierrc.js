c215a6dc27b200e37df1737f7d4be484fc2eedf9
Jille Borg <jille.borg@npo.nl>
2024-12-29 11:32:21 +0100

    feat: init new next.js project

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  jsxSingleQuote: true,
+  printWidth: 120,
+  quoteProps: 'consistent',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
