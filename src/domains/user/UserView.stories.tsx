b7d748e2e1df71e5543e669e9dbc2fd5557f6a74
Jille Borg <jille.borg@npo.nl>
2024-12-30 12:51:03 +0100

    feat: Resolve pre-push

--- src/domains/user/UserView.stories.tsx
+++ src/domains/user/UserView.stories.tsx
@@ -1,10 +1,10 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import { UserView, UserViewProps } from './UserView'
+import { UserView } from './UserView'
 
-export default { component: UserView } as Meta<UserViewProps>
+export default { component: UserView } as Meta
 
-export const Primary: StoryObj<UserViewProps> = {
+export const Primary: StoryObj = {
   args: {},
   argTypes: {}
 }
